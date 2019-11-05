2a965a507aeaf6a2c32c66cb059fcdf4bcd20f06
aminamaludin <38432848+aminamaludin@users.noreply.github.com>
2019-11-05 16:05:36 +0700

    Update footer.php
    mengubah nama instansi

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,6 @@
 <div class="footer-copyright">
     <div class="container center">
-        <br>© 2019 Copyright SMK Negeri 7 Purworejo
+        <br>© 2019 Copyright SMK One Piece
         <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
     </div>
 </div>
